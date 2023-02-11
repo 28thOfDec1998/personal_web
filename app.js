c61e4c682e1a7a13e6fca93f7abca6bbbb22dabf
Calvin3c <englandisme123@gmail.com>
2023-02-11 21:30:12 +0500

    .

---
+++ app.js
@@ -0,0 +1,13 @@
+//  let myAge = 10
+
+//  myAge = myAge + 10 
+
+// console.log(myAge)
+
+// let first_num = 5
+
+// let second_num = 6
+
+// let final_num = first_num * second_num
+
+// console.log(final_num)
