f8b9352da6739d810f4d6ddbd7216c5aede29efe
Calvin3c <englandisme123@gmail.com>
2023-02-15 20:12:31 +0500

    .

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 let count = 0;
-let countEl = document.getElementById("count-el"); 
+let countEl = document.getElementById("count-el");
+let saveEl = document.getElementById("save-el");
+
+console.log(saveEl)
 
 
 
 function increment() {
     count += 1;
-    countEl.innerText = count;
+    countEl.textContent = count;
 }
 function save (){
+    let countStr = count + " - ";
+
+    saveEl.textContent += countStr;
     
+    countEl.textContent = 0 ;
+
+    count = 0;
 }
 
